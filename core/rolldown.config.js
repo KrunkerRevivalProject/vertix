f0d04ca8038f6c07c46dca6a74077387aeb02a9d
Gus <58223632+thegu5@users.noreply.github.com>
2026-02-28 01:51:59 -0500

    simplify package.json, cleanup configs

--- core/rolldown.config.js
+++ core/rolldown.config.js
@@ -1,7 +1,6 @@
 import { join } from "node:path";
 import { defineConfig } from "rolldown";
 
-console.log(join(import.meta.dirname, "../public/js"));
 export default defineConfig({
 	input: join(import.meta.dirname, "src/app.ts"),
 	output: {
